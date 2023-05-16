fddba87ab47fabfdf596477f9bff1276b57c66b5
Susmit Barua <111695274+Susmit-Barua@users.noreply.github.com>
2023-05-16 21:08:45 +0600

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+<<<<<<< HEAD
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "21d3da1010msh5f308d34960a8dap12531djsnd91ef369f767",
+=======
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': "21d3da1010msh5f308d34960a8dap12531djsnd91ef369f767" ,
+>>>>>>> e069d25f40bde1990a18dbe0ae2fe2995c226e79
+    },
+  });
+
+  return data;
+<<<<<<< HEAD
+};
+=======
+}
+>>>>>>> e069d25f40bde1990a18dbe0ae2fe2995c226e79
